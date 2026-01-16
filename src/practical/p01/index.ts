ada16a75944abc9177bb9691c9e76708f5bd2319
Kuuhakumu <68010547@kmitl.ac.th>
2026-01-16 19:32:05 +0700

    yes

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,14 @@
-export function getPostalAddress() {}
+import axios from "axios"
+export const fetch = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.data)
+    .catch(error => console.error('Error fetching data:', error));
+    return res;
+}
+export function getPostalAddress() {
+    return fetch().then(users => {
+        return users.map((user: { address: string}) => {
+            return user.address;
+        });
+});
+}
